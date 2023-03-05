c973d24ce7ee9af0a0ce7be2451df16bc434a2dd
okawahiroto <okawa.hiroto@gmail.com>
2023-03-05 16:37:38 +0900

    単音に変更

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const buttonOn = document.getElementById("buttonOn");
 const buttonStop = document.getElementById("buttonStop");
 
 let oscNode = new OscillatorNode(audioCtx, { type: 'sawtooth', frequency: 442 });
-let oscNode2 = new OscillatorNode(audioCtx, { type: 'sawtooth', frequency: 500 });
+// let oscNode2 = new OscillatorNode(audioCtx, { type: 'sawtooth', frequency: 500 });
 
 let gainNode = new GainNode(audioCtx, { gain: 0.1 });
 let gainNode2 = new GainNode(audioCtx, { gain: 0.1 });
@@ -17,10 +17,10 @@ buttonOn.addEventListener("click", function () {
   console.log("buttonOn clicked");
   oscNode.connect(gainNode);
   gainNode.connect(audioCtx.destination);
-  oscNode2.connect(gainNode2);
-  gainNode2.connect(audioCtx.destination);
+  // oscNode2.connect(gainNode2);
+  // gainNode2.connect(audioCtx.destination);
   oscNode.start();
-  oscNode2.start();
+  // oscNode2.start();
 });
 
 //Offボタンクリック時
@@ -29,10 +29,10 @@ buttonStop.addEventListener("click", function () {
   oscNode.connect(gainNode);
   gainNode.connect(audioCtx.destination);
   oscNode.stop();
-  oscNode2.stop();
+  // oscNode2.stop();
   // OscillatorNodeオブジェクトを再生成
   oscNode = new OscillatorNode(audioCtx, { type: 'sawtooth', frequency: 442 });
-  oscNode2 = new OscillatorNode(audioCtx, { type: 'sawtooth', frequency: 500 });
+  // oscNode2 = new OscillatorNode(audioCtx, { type: 'sawtooth', frequency: 500 });
 });
 
 // range変更時の値取得
