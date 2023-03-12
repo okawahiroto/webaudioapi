14b8ba099e9f568732cf52266a4e0f92ea31ef08
okawahiroto <okawa.hiroto@gmail.com>
2023-03-12 17:45:40 +0900

    三和音追加、長調・短調機能追加

--- app.js
+++ app.js
@@ -1,60 +1,111 @@
 console.log("index.js loaded");
 
-const audioCtx = new AudioContext();
-
 const rangeFrequency = document.getElementById("rangeFrequency");
+const radioMajor = document.getElementById("radioMajor");
+const radioMinor = document.getElementById("radioMinor");
 const buttonOn = document.getElementById("buttonOn");
-const buttonAdd = document.getElementById("buttonAdd");
+const buttonThird = document.getElementById("buttonThird");
+const buttonFifth = document.getElementById("buttonFifth");
 const buttonOff = document.getElementById("buttonOff");
 const buttonChange = document.getElementById("buttonChange");
 
-console.log(rangeFrequency.value);
+console.log('range:' + rangeFrequency.value);
+console.log('radioMajor:' + radioMajor.checked);
 
-let oscNode = new OscillatorNode(audioCtx, { type: 'sine', frequency: rangeFrequency.value });
+// 各オシレーターの周波数を計算
+let rootFrequency = rangeFrequency.value;
+let third = 5/4;
+let fifth = 3/2;
 
-let gainNode = new GainNode(audioCtx, { gain: 0.1 });
-let gainNode2 = new GainNode(audioCtx, { gain: 0.1 });
+let frequency01 = 440;
+let frequency02 = rootFrequency * third;
+let frequency03 = rootFrequency * fifth;
 
-buttonOff.disabled = true;
+// ラジオボタンから長調か短調か判断。デフォルトは長調
+function keyCheck(event) {
+  if (event.target.id === "radioMajor") {
+    console.log("radioMajor");
+    third = 5/4;
+    fifth = 3/2;
+    keyChange();
+    console.log("frequency02:" + frequency02);
+  } else if(event.target.id === "radioMinor") {
+    console.log("radioMinor");
+    third = 6/5;
+    fifth = 3/2;
+    keyChange();
+    console.log("frequency02:" + frequency02);
+  }
+};
+
+// 長調・短調が変わると和音(周波数)を計算し直す。
+function keyChange() {
+  frequency02 = rootFrequency * third;
+  frequency03 = rootFrequency * fifth;
+};
+
+
+radioMajor.addEventListener("change", keyCheck);
+radioMinor.addEventListener("change", keyCheck);
+
+// range変更時の値取得
+rangeFrequency.addEventListener('input', inputChange);
+function inputChange() {
+  rootFrequency = rangeFrequency.value;
+  frequency01 = rangeFrequency.value;
+  console.log('freq01:' + frequency01);
+};
+
+console.log('freq01:' + frequency01);
+
+// オシレーターを生成
+let audioCtx = new AudioContext();
+const gainNode = audioCtx.createGain();
+const oscillator01 = audioCtx.createOscillator();
+const oscillator02 = audioCtx.createOscillator();
+const oscillator03 = audioCtx.createOscillator();
+
+// オシレーターの設定
+
+
+// ゲインノードの設定
+gainNode.gain.value = 0.1;
+
+// 各オシレーターを出力先に接続して、再生開始
+gainNode.connect(audioCtx.destination);
+
 
 //Onボタンクリック時
 buttonOn.addEventListener("click", function () {
   console.log("On");
-  console.log(rangeFrequency.value);
+  console.log("frequency01:" + frequency01);
+  oscillator01.frequency.value = frequency01;
+  oscillator01.type = "sine";
+  oscillator01.connect(gainNode);
+  oscillator01.start();
+});
 
-  oscNode.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
-  oscNode.start();
+buttonThird.addEventListener("click", function() {
+  console.log("Third");
+  console.log("frequency02:" + frequency02);
+  oscillator02.frequency.value = frequency02;
+  oscillator02.type = "sine";
+  oscillator02.connect(gainNode);
+  oscillator02.start();
+});
 
-  buttonOn.disabled = true;
-  buttonOff.disabled = false;
-  rangeFrequency.disabled = true;
+buttonFifth.addEventListener("click", function() {
+  console.log("Fifth");
+  console.log("frequency03:" + frequency03);
+
+  oscillator03.frequency.value = frequency03;
+  oscillator03.type = "sine";
+  oscillator03.connect(gainNode);
+  oscillator03.start();
 });
 
 //Offボタンクリック時
 buttonOff.addEventListener("click", function () {
   console.log("Off");
-
-  oscNode.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
-  oscNode.stop();
-
-  oscNode = new OscillatorNode(audioCtx, { type: 'sine', frequency: rangeFrequency.value });
-
-  buttonOn.disabled = false;
-  buttonOff.disabled = true;
-  rangeFrequency.disabled = false;
-});
-
-// range変更時の値取得
-rangeFrequency.addEventListener('input', inputChange);
-function inputChange() {
-  console.log(rangeFrequency.value);
-  oscNode = new OscillatorNode(audioCtx, { type: 'sine', frequency: rangeFrequency.value });
-};
-
-buttonAdd.addEventListener("click", function() {
-  console.log("Add");
-  // oscNode = new OscillatorNode(audioCtx, { type: 'sine', frequency: rangeFrequency.value });
-  // console.log(rangeFrequency.value);
+  location.reload()
 });
