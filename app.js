39a8df2d768fecd7d1d0634a9d047e88fc80eb36
okawahiroto <okawa.hiroto@gmail.com>
2023-03-19 09:40:30 +0900

    調整・根音を選択時の処理を関数化。(ChatGPTを利用)

--- app.js
+++ app.js
@@ -1,84 +1,96 @@
-console.log("index.js loaded");
+console.log("app.js loaded");
 
 const rangeFrequency = document.getElementById("rangeFrequency");
 const radioMajor = document.getElementById("radioMajor");
 const radioMinor = document.getElementById("radioMinor");
+
 const buttonOn = document.getElementById("buttonOn");
 const buttonThird = document.getElementById("buttonThird");
 const buttonFifth = document.getElementById("buttonFifth");
 const buttonOff = document.getElementById("buttonOff");
 const buttonChange = document.getElementById("buttonChange");
 
-console.log('range:' + rangeFrequency.value);
-console.log('radioMajor:' + radioMajor.checked);
+let selectedKey = "keyMajor";
+let selectedNote = "A";
+
+// ChatGPTのコード。3つの周波数の配列を準備、calculateFrequencies関数で調性と根音に基づき、3つの周波数を配列で返し(代入し)ている。
+let [frequency01, frequency02, frequency03] = calculateFrequencies(selectedKey, selectedNote);
+
+// 初期値を設定する方法。自分だとこうする。ChatGPTのようなコードを書けるようになりたい。
+// let [frequency01, frequency02, frequency03] = [440, 550, 660];
+
+console.log(selectedKey + ':' + selectedNote);
 
 // 各オシレーターの周波数を計算
-let rootFrequency = rangeFrequency.value;
-let third = 5/4;
-let fifth = 3/2;
-
-let frequency01 = 440;
-let frequency02 = rootFrequency * third;
-let frequency03 = rootFrequency * fifth;
-
-// ラジオボタンから長調か短調か判断。デフォルトは長調
-function keyCheck(event) {
-  if (event.target.id === "radioMajor") {
-    console.log("radioMajor");
+function calculateFrequencies(key, note) {
+  let third, fifth;
+  if (key === "keyMajor") {
     third = 5/4;
     fifth = 3/2;
-    keyChange();
-    console.log("frequency02:" + frequency02);
-  } else if(event.target.id === "radioMinor") {
-    console.log("radioMinor");
+  } else {
     third = 6/5;
     fifth = 3/2;
-    keyChange();
-    console.log("frequency02:" + frequency02);
   }
-};
-
-function getSelectedOption() {
-  var options = document.getElementsByName("exampleRadios");
-  var selectedOption = [];
-
-  for (var i = 0; i < options.length; i++) {
-    if (options[i].checked) {
-      selectedOption.push(options[i].value);
-    }
+  let frequency01, frequency02, frequency03;
+  switch(note) {
+    case "A":
+      frequency01 = 440;
+      break;
+    case "C":
+      frequency01 = 261.63;
+      break;
+    case "F":
+      frequency01 = 349.23;
+      break;
+    case "Bb":
+      frequency01 = 466.16;
+      break;
+    default:
+      frequency01 = 440;
   }
-
-  return selectedOption;
+  frequency02 = frequency01 * third;
+  frequency03 = frequency01 * fifth;
+  return [frequency01, frequency02, frequency03];
 }
 
-var options = document.getElementsByName("options");
+// 調性・根音変更時の処理
+function updateFrequencies() {
+  console.log(`選択された調性は${selectedKey}です。`);
+  console.log(`選択された音は${selectedNote}です。`);
+  const frequencies = calculateFrequencies(selectedKey, selectedNote);
+  frequency01 = frequencies[0];
+  frequency02 = frequencies[1];
+  frequency03 = frequencies[2];
+  console.log(`frequency: ${frequency01}, ${frequency02}, ${frequency03}`);
+}
 
-for (var i = 0; i < options.length; i++) {
-  options[i].addEventListener("click", function() {
-    var selected = getSelectedOption();
-    console.log(selected); // 選択されたラジオボタンの値が格納された配列が表示される
+// 調性を選んだとき
+const radioKeys = document.querySelectorAll('input[name="key"]');
+for (const radio of radioKeys) {
+  radio.addEventListener('change', (event) => {
+    selectedKey = event.target.value;
+    updateFrequencies();
+    console.log(selectedKey + ':' + selectedNote);
   });
-};
+}
 
-// 長調・短調が変わると和音(周波数)を計算し直す。
-function keyChange() {
-  frequency02 = rootFrequency * third;
-  frequency03 = rootFrequency * fifth;
-};
-
-
-radioMajor.addEventListener("change", keyCheck);
-radioMinor.addEventListener("change", keyCheck);
+// 根音を選んだときの処理
+const radioNotes = document.querySelectorAll('input[name="note"]');
+for (const radio of radioNotes) {
+  radio.addEventListener('change', (event) => {
+    selectedNote = event.target.value;
+    updateFrequencies();
+    console.log(selectedKey + ':' + selectedNote);
+  });
+}
 
 // range変更時の値取得
 rangeFrequency.addEventListener('input', inputChange);
 function inputChange() {
   rootFrequency = rangeFrequency.value;
   frequency01 = rangeFrequency.value;
-  console.log('freq01:' + frequency01);
+  console.log('frequency01:' + frequency01);
 };
-
-console.log('freq01:' + frequency01);
 
 // オシレーターを生成
 let audioCtx = new AudioContext();
